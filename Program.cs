1a6d82d897090ba8c6f9039a1bfc42ad48d1f0fd
RichaSingh00 <ririco613@gmail.com>
2023-09-07 17:24:33 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = args[0].Split(',');
+              
+            string developerName = input [0];
+            string companyName = input[1];
+            string location = input[2];
+
+            Console.WriteLine("Developer name is: " + developerName);
+            Console.WriteLine("Company name is: " + companyName);
+            Console.WriteLine("Location is: " + location);
+        }
+    }
+}
